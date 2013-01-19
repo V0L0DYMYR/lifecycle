eed074d0fae368f128dcb478139b31201d8bf3e4
Volodymyr Derkach <derkach.vladimir@yahoo.com>
2013-01-19 19:20:52 +0200

    worked View

--- lifecycle-web/src/js/main.js
+++ lifecycle-web/src/js/main.js
@@ -3,12 +3,31 @@ var Ticket = Backbone.Model.extend({
             id: null,
             title: 'default title'
     }
-
 });
-var ticket = new Ticket();
 
 var TicketList = Backbone.Collection.extend({
     model: Ticket,
     url: "http://localhost:8000/ticket"
 });
-var ticketList = new TicketList();
+
+var TicketView = Backbone.View.extend({
+    template: '#ticket_tmpl',
+    render: function(){
+        var tmplMarkup = $(this.template).html();
+        var tmplCompiled =_.template(tmplMarkup, {model:this.model.toJSON()});
+        $(this.el).append(tmplCompiled);
+        return this;
+    }
+});
+
+var TicketListView = Backbone.View.extend({
+    el:'#ticket_list',
+    render: function(){
+        var that = this;
+        var el = $(this.el);
+        el.empty();
+        this.collection.each(function(model){
+            el.append(new TicketView({model:model, el:that.el}).render());
+        });
+    }
+});
